2f9b17d7e0af6c4b4c786b1f7231799e901b4eb7
CodeSpitterKEa <olivervinstedi@gmail.com>
2026-03-02 13:03:36 +0100

    Setup

---
+++ src/main/java/com/example/kinoxp/repository/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.example.kinoxp.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MovieRepository {
+}
---
+++ src/main/java/com/example/kinoxp/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.kinoxp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
