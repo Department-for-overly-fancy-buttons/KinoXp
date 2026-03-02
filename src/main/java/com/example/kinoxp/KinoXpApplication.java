df22e1ff3d080c5413ce89a2a21af51fced86fbd
CodeSpitterKEa <olivervinstedi@gmail.com>
2026-03-02 11:39:48 +0100

    Setup

---
+++ src/main/java/com/example/kinoxp/KinoXpApplication.java
@@ -0,0 +1,12 @@
+package com.example.kinoxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXpApplication {
+    static void main(String[] args) {
+        SpringApplication.run(KinoXpApplication.class, args);
+
+    }
+}
---
+++ src/test/java/com/example/kinoxp/KinoXpApplicationTest.java
@@ -0,0 +1,7 @@
+package com.example.kinoxp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KinoXpApplicationTest {
+
+}
