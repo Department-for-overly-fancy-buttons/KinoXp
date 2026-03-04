c3978b5e694f8c58edcc7572b68f610aebc0944d
Frede-72 <frjo0005@stud.ek.dk>
2026-03-04 12:53:01 +0100

    REMOVED: test

---
+++ src/main/java/com/example/kinoxp/repository/ReservationRepository.java
@@ -0,0 +1,7 @@
+package com.example.kinoxp.repository;
+
+import com.example.kinoxp.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
