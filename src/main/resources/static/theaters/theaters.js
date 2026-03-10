a6ba345b8b8feca7995c780c22a5bc8d3027521b
Frede-72 <frjo0005@stud.ek.dk>
2026-03-10 11:39:32 +0100

    fetch showings for specific theater

--- src/main/resources/static/theaters/theaters.js
+++ src/main/resources/static/theaters/theaters.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", initApp);
 
-import {displayShowings, handleGetTickets,BASE_URL} from "../app";
-
+const BASE_URL = "http://localhost:8080/api";
+const params = new URLSearchParams(window.location.search);
 const theaterId = params.get("theaterId");
 let movieContainerEl = document.querySelector("#movie-container");
 let showingsData = [];
 
 async function initApp() {
     showingsData = await fetchShowings();
+    console.log(showingsData);
     displayShowings(showingsData);
     movieContainerEl.addEventListener("click", handleGetTickets);
 }
 
 async function fetchShowings() {
     try {
-        const response = await fetch(`${BASE_URL}/theaters/showings${theaterId}`);
+        const response = await fetch(`${BASE_URL}/theaters/showings/${theaterId}`);
         if (!response.ok) {
             throw new Error("HTTP error!");
         }
@@ -25,4 +26,66 @@ async function fetchShowings() {
         console.log("An error occurred:   " + error)
     }
 
+}
+
+async function handleGetTickets(event) {
+    event.preventDefault();
+
+    const movieBox = event.target.closest("div");
+    const showingId = movieBox.getAttribute("data-showingId");
+    if (showingId !== null) {
+        console.log("clicked showing with id = " + showingId);
+        window.location.href = `Reservations/Reservation.html?showingId=${showingId}`;
+    } else {
+        console.log("box clicked");
+    }
+}
+
+function displayShowings(showings) {
+    movieContainerEl.innerHTML = "";
+    for (let showing of showings) {
+        let movieBoxEl = document.createElement("div");
+        movieBoxEl.classList.add("movie-box");
+        movieBoxEl.setAttribute("data-showingID", showing.id);
+
+        let titleElement = document.createElement("h2");
+        titleElement.textContent = `${showing.movie.title}`;
+        movieBoxEl.appendChild(titleElement);
+
+        //let svgCoverImageElement = document.createElement("svg");
+        //let backgroundRectElement = document.createElement("rect");
+        //svgCoverImageElement.width = 100;
+
+
+        let coverImageElement = document.createElement("img");
+        coverImageElement.alt = "Missing cover image";
+        coverImageElement.src = "/images/Missing_Cover_Image.png";
+        coverImageElement.classList.add("cover-image");
+        movieBoxEl.appendChild(coverImageElement);
+
+        let infoBarElement = document.createElement("h4");
+        infoBarElement.textContent = `${showing.movie.duration} min, PG: ${showing.movie.pgRating}`;
+        movieBoxEl.appendChild(infoBarElement);
+
+        let categoryElement = document.createElement("h3");
+        let categorySentencce = ``;
+        let categories = showing.movie.categories;
+        console.log(categories)
+        for (category of categories) {
+            categorySentencce += `|${category.genre}|`;
+            console.log(categorySentencce)
+        }
+        categoryElement.textContent = categorySentencce;
+        movieBoxEl.appendChild(categoryElement);
+
+        let descriptionElement = document.createElement("p");
+        descriptionElement.textContent = `${showing.movie.description}`;
+        movieBoxEl.appendChild(descriptionElement);
+
+        let getTicketButton = document.createElement("button");
+        getTicketButton.textContent = "Get tickets!";
+        movieBoxEl.appendChild(getTicketButton);
+
+        movieContainerEl.appendChild(movieBoxEl);
+    }
 }
