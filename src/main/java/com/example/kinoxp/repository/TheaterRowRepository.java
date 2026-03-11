fe0817be0f311bf0355a972bb6dae2fc4b71f2ad
Frede-72 <frjo0005@stud.ek.dk>
2026-03-11 10:50:26 +0100

    ADDED: TheaterRow.java add custom ticketttype to every row

---
+++ src/main/java/com/example/kinoxp/repository/TheaterRowRepository.java
@@ -0,0 +1,7 @@
+package com.example.kinoxp.repository;
+
+import com.example.kinoxp.model.TheaterRow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheaterRowRepository extends JpaRepository<TheaterRow, Long> {
+}
