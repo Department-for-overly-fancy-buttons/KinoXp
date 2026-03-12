ede7194a179f9d86adb41e2d539ade46de897744
Frede-72 <frjo0005@stud.ek.dk>
2026-03-12 10:33:15 +0100

    ADDED: feetype repository

--- src/main/java/com/example/kinoxp/model/FeeType.java
+++ src/main/java/com/example/kinoxp/model/FeeType.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class FeeTypes {
+public class FeeType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String type;
