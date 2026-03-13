d4ad2e6d3b6d82a657b378f3c34b69a5815a50f8
Frede-72 <frjo0005@stud.ek.dk>
2026-03-13 13:18:40 +0100

    Simplified (in my opinion) priceCalculationTotal

--- src/main/java/com/example/kinoxp/service/ReservationService.java
+++ src/main/java/com/example/kinoxp/service/ReservationService.java
@@ -53,53 +53,26 @@ public class ReservationService {
 
     public double priceCalculationTotal(Reservation reservation) {
         double total = 0;
+        int numberOfTickets = reservation.getTickets().size();
 
         for (Ticket ticket : reservation.getTickets()) {
             total += ticket.getTicketType().getPrice();
-            System.out.println("Ticket - " + ticket.getSeatLabel() + " - __________ " + ticket.getTicketType().getPrice() + "kr.");
-            if (reservation.getShowing().getMovie().getDuration() > 170) {
-                total += feeRepository.findById("hel_aften_film").get().getPrice();
-                System.out.println("HelAftens film gebyr: __________ 30kr. \nSubtotal __________ " + total + "kr.");
-            }
-            if (reservation.getShowing().isThreeDimensional()) {
-                total += feeRepository.findById("3d").get().getPrice();
-                System.out.println("3D gebyr: __________ " + feeRepository.findById("3d").get().getPrice() + "kr. \nSubtotal __________ " + total + "kr.");
-            }
         }
-        System.out.println("Subtotal __________ " + total + "kr.");
 
-        double fee = feeHandler(reservation);
-        double discount = groupDiscount(reservation, total);
+        if (reservation.getShowing().getMovie().getDuration() > 170) {
+            total += (feeRepository.findById("hel_aften_film").get().getPrice() * numberOfTickets);
+        }
+        if (reservation.getShowing().isThreeDimensional()) {
+            total += (feeRepository.findById("3d").get().getPrice() * numberOfTickets);
+        }
 
-//        System.out.println("Reservation gebyr:  __________ " + fee + "kr. \nSubtotal __________ " + (total + fee) + "kr.");
-        System.out.println("Reservation gebyr eller Rabat:  __________" + discount + "kr. \nSubtotal __________ " + (total + discount) + "kr.");
-        System.out.println("Total __________ " + (total + discount) + "kr.");
-        return total + discount;
+        if (numberOfTickets < 5) {
+            total += feeRepository.findById("reservationsgebyr").get().getPrice();
+        } else if (numberOfTickets > 10) {
+            double discount = total * 0.07;
+            total -= discount;
+        }
+        return total;
 
     }
-    private double feeHandler(Reservation reservation) {
-        double reservationfee = 0;
-        if (reservation.getTickets().size() < 5) {
-            return feeRepository.findById("reservationsgebyr").get().getPrice();
-        }
-        return 0;
-
-    }
-   private double groupDiscount(Reservation reservation, double subtotal) {
-       if (reservation.getTickets() == null) {
-           return 0;
-       }
-       int numberOfTickets = reservation.getTickets().size();
-       if (numberOfTickets > 10) {
-//           double totalTicketPrice = 0;
-//
-//           for (Ticket ticket : reservation.getTickets()) {
-//               totalTicketPrice += ticket.getTicketType().getPrice();
-//           }
-           return -(subtotal * 0.07);
-       }else if(reservation.getTickets().size() < 5){
-           return feeRepository.findById("reservationsgebyr").get().getPrice();
-       }
-       return  0;
-   }
- }
+}
