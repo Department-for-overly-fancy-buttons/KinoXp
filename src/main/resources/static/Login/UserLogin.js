eba73413f27a0e49bb07ad7a55b510f0edc7276c
Frede-72 <frjo0005@stud.ek.dk>
2026-03-16 17:17:42 +0100

    ADDED: module for import

--- src/main/resources/static/Login/UserLogin.js
+++ src/main/resources/static/Login/UserLogin.js
@@ -1,6 +1,9 @@
+import {displayNavigationBar} from "./navigationBars.js";
+
 const BASE_URL = "http://localhost:8080/api/users"
 
 document.addEventListener('DOMContentLoaded', () => {
+    displayNavigationBar();
     const loginForm = document.getElementById("loginForm");
     if (!loginForm) return;
 
