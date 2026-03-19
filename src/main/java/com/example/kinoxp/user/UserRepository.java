3a4d0c2fb15abd19f730a39a8c2d884cdcf7f1be
Frede-72 <frjo0005@stud.ek.dk>
2026-03-19 15:14:24 +0100

    small refactor

--- src/main/java/com/example/kinoxp/user/UserRepository.java
+++ src/main/java/com/example/kinoxp/user/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+interface UserRepository extends JpaRepository<User,Long> {
 
     List<User> findByRole(String role);
 
